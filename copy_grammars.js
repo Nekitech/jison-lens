579f296d8ca06bdbe12d381646395c68ed034c65
Nekitech <nekitech-x@mail.ru>
2025-03-24 19:42:38 +0300

    fix

--- copy_grammars.js
+++ copy_grammars.js
@@ -1,8 +1,10 @@
+import fs from 'fs-extra';
+
 function copy_grammar() {
-  const grammars_input_path = path.resolve(process.cwd(), 'src/shared/grammars');
+  const grammars_input_path = path.resolve(process.cwd(), 'src/shared/grammars/');
   const output_path = '/tmp/';
   fs.copy(grammars_input_path, output_path, (err) => {
     if (err) return console.error('Ошибка копирования:', err);
-    console.log('Папка templates успешно скопирована!');
+    console.log('Папка успешно скопирована!');
   });
 }
