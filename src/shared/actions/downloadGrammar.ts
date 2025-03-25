a3d3142b3413cdb086fe310a37504915445f92e2
Nekitech <nekitech-x@mail.ru>
2025-03-25 13:17:59 +0300

    fix

--- src/shared/actions/downloadGrammar.ts
+++ src/shared/actions/downloadGrammar.ts
@@ -6,8 +6,8 @@ import * as fs from 'node:fs'
 export default async function downloadGrammar(file: string) {
   const isProduction = process.env.NODE_ENV === 'production';
   const file_path = isProduction
-    ? path.join('/tmp/', file)
-    : path.join(process.cwd(), 'src/shared/grammars/', file)
+    ? path.resolve('/tmp/', file)
+    : path.resolve(process.cwd(), 'src/shared/grammars/', file)
 
   return fs.readFileSync(file_path, 'utf8')
 }  
